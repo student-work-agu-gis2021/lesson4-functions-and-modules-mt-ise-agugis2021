157f6e168c5cfa05069bb49652a59a408387d76b
mtiseagugis2021 <>
2021-04-30 10:10:52 +0000

    I added  comments.

--- temp_functions.py
+++ temp_functions.py
@@ -1,12 +1,15 @@
 def fahr_to_celsius(temp_fahrenheit):
   return(temp_fahrenheit-32)/1.8
 
-  def temp_classifier(temp_celsius):
-   if(temp_celsius<-2):
-    return 0
-   elif(temp_celsius>=-2)and(temp_celsius<2):
+def temp_classifier(temp_celsius):
+  #The purpose of function : Separate the Temperatures
+  #Paramater: temp_celsius
+  #4 classes from 0 to 3
+  if temp_celsius<-2:
+   return 0
+  elif temp_celsius<2:
     return 1
-   elif(temp_celsius>=2)and(temp_celsius<15):
-      return 2
-   elif(temp_celsius>=15):
-      return 3
+  elif temp_celsius<15:
+    return 2
+  elif temp_celsius>=15:
+    return 3
