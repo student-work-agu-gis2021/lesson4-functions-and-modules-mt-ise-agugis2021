c7221f826a4265e93da8481709d3277445723560
mtiseagugis2021 <>
2021-04-30 09:58:34 +0000

    I changed codes.

--- Exercise_4_problem_2.py
+++ Exercise_4_problem_2.py
@@ -26,14 +26,14 @@
 
 # YOUR CODE HERE to define temp_classifier
 def temp_classifier(temp_celsius):
-  if(temp_celsius<-2):
+  if temp_celsius<-2:
    return 0
-  elif(temp_celsius>=-2)and(temp_celsius<2):
+  elif temp_celsius<2:
     return 1
-  elif(temp_celsius>=2)and(temp_celsius<15):
-      return 2
-  elif(temp_celsius>=15):
-      return 3
+  elif temp_celsius<15:
+    return 2
+  elif temp_celsius>=15:
+    return 3
 # Use your newly created function to print the answers to the following questions:
 
  # Reclassify temperature.
