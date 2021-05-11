d34c9bfcbb8e4b86af0e2dabfff0295cb912732b
mtiseagugis2021 <>
2021-05-11 02:27:02 +0000

    I added docstring.

--- temp_functions.py
+++ temp_functions.py
@@ -1,13 +1,13 @@
 def fahr_to_celsius(temp_fahrenheit):
-  #TO change from fahrenheit to celsius
-  #temp_fahrenheit: The temperature to change.
+  """TO change from fahrenheit to celsius
+  temp_fahrenheit: The temperature to change."""
   converted_temp = (temp_fahrenheit-32)/1.8
   return converted_temp
 
 def temp_classifier(temp_celsius):
-  #The purpose of function : Separate the Temperatures
-  #Paramater: temp_celsius
-  #4 classes from 0 to 3
+  """The purpose of function : Separate the Temperatures
+  Paramater: temp_celsius
+  returned values: 4 classes from 0 to 3"""
   if temp_celsius<-2:
    return 0
   elif temp_celsius<2:
